0d1309ade6a171e59c1d2007d52b7cf1bcd721b7
Calvin Philmore <74697299+sphilmore@users.noreply.github.com>
2022-07-12 10:52:41 -0400

    Update main.py
    I changed the code to follow OOP format.

--- main.py
+++ main.py
@@ -1,41 +1,46 @@
 import requests
 from twilio.rest import Client
 import os
-from dotenv import load_dotenv
-load_dotenv("variables\.env.txt")
-KEYS = os.getenv("api_key")
-ACCOUNT = os.getenv("account_sid")
-TOKEN = os.getenv("auth_token")
-lat =39.960491
-lon = -75.262779
-parameters ={
-    "lat": lat,
-    "lon": lon,
-    "appid": KEYS,
-    "exclude": "current,minutely,daily,"
-}
-response = requests.get(url="https://api.openweathermap.org/data/2.5/onecall", params=parameters)
-response.raise_for_status()
-data = response.json()
-will_rain = False
-for things in range(12):
-    save_two = (data["hourly"][things]["weather"][0]["id"])
-    if save_two <= 700:
-      will_rain = True
-    else:
-      will_rain = False
+KEYS = os.getenv('weather_api_key')
+ACCOUNT = os.getenv('twilio_account')
+TOKEN = os.getenv('twilio_token')
+NUMBER = os.getenv('phone_number')
+API_END_POINT =os.getenv('weather_end_point')
+class WeatherAlert:
+    def __init__(self):
+        self.latitude = 39.960491
+        self.longitude = -75.262779
+        self.will_rain =False
+    def get_weather_data(self):
+        parameters = {
+            "lat": self.latitude,
+            "lon": self.longitude,
+            "appid": KEYS,
+            "exclude": "current,minutely,daily,"
+        }
+        response = requests.get(url=API_END_POINT, params=parameters)
+        response.raise_for_status()
+        data = response.json()
+        for things in range(12):
+            save_two = (data["hourly"][things]["weather"][0]["id"])
+            if save_two <= 700:
+                self.will_rain = True
+            else:
+                self.will_rain = False
+    def notification(self):
 
-if will_rain == True:
-    client = Client(ACCOUNT, TOKEN)
-    message = client.messages \
-        .create(
-        body="It's going to rain today. Bring an umberella ☂",
-        from_='+19897621014',
-        to='+12158239955'
+        if self.will_rain == True:
+            client = Client(ACCOUNT, TOKEN)
+            message = client.messages \
+            .create(
+            body="It's going to rain today. Bring an umberella ☂",
+            from_='+19897621014',
+            to=NUMBER
     )
-    print(message.sid)
-else:
-    pass
+            print(message.sid)
+        else:
+            pass
 
-
-
+rain_alert = WeatherAlert()
+rain_alert.get_weather_data()
+rain_alert.notification()
